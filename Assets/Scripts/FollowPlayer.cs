718c0237ef79e4ad33b1d691b2f1abeee7d2f87a
gnut06300 <gnut@gnut.eu>
2022-12-21 10:11:28 +0100

    Change Scripts FolowsPlayer and PlayerController

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
-
+    // private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
